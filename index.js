55df29a84ca1fc7c3934f2e505d019bae916aeb7
Eli <136011323+EliasElliotson@users.noreply.github.com>
2025-05-08 00:40:04 +0000

    Added vector math

--- index.js
+++ index.js
@@ -90,6 +90,18 @@ class Vector {
   toArray() {
     return [this.x, this.y];
   }
+
+  static add(vec1, vec2) {
+    return new Vector(vec1.x + vec2.x, vec1.y + vec2.y);
+  }
+
+  static sub(vec1, vec2) {
+    return new Vector(vec1.x - vec2.x, vec1.y - vec2.y);
+  }
+
+  static dot(vec1, vec2) {
+    return (vec1.x * vec2.x) + (vec1.y * vec2.y);
+  }
 }
 
 export { Vector as default, Vector, Vector as Vec};
